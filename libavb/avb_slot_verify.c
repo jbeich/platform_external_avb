d22499bd7a68285f09907920d289a87602d1aa0b
Bowgo Tsai <bowgotsai@google.com>
2018-12-04 13:13:02 +0800

    Fix AvbSlotVerifyData->cmdline might be NULL
    When AVB_VBMETA_IMAGE_FLAGS_VERIFICATION_DISABLED is set,
    AvbSlotVerifyData->cmdline should be a NUL-terminated string instead of
    NULL.
    
    Bug: 119551429
    Test: atest libavb_host_unittest
    
    Change-Id: I27616b217f01acf7e1196bacc023ea610117935c

--- libavb/avb_slot_verify.c
+++ libavb/avb_slot_verify.c
@@ -1442,10 +1442,13 @@ AvbSlotVerifyResult avb_slot_verify(AvbOps* ops,
       if (has_system_partition(ops, ab_suffix)) {
         slot_data->cmdline =
             avb_strdup("root=PARTUUID=$(ANDROID_SYSTEM_PARTUUID)");
-        if (slot_data->cmdline == NULL) {
-          ret = AVB_SLOT_VERIFY_RESULT_ERROR_OOM;
-          goto fail;
-        }
+      } else {
+        // The |cmdline| field should be a NUL-terminated string.
+        slot_data->cmdline = avb_strdup("");
+      }
+      if (slot_data->cmdline == NULL) {
+        ret = AVB_SLOT_VERIFY_RESULT_ERROR_OOM;
+        goto fail;
       }
     } else {
       /* If requested, manage dm-verity mode... */
