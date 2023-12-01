1fb850157c451a5bb9060fe77c851dc619fa7aaf
David Pursell <dpursell@google.com>
2023-11-30 16:30:22 -0800

    libavb_rs: add hash descriptor support
    Adds the HashDescriptor struct which provides a safe API to access hash
    descriptors.
    
    Bug: b/290110273
    Test: atest
    Change-Id: Ie920b9d5358c45b7f6fdb3c5793826014f4084b8

---
+++ rust/src/descriptor/hash.rs
@@ -0,0 +1,177 @@
+// Copyright 2023, The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Hash descriptors.
+
+use super::{util::extract_slice, DescriptorError, DescriptorResult};
+use avb_bindgen::{avb_hash_descriptor_validate_and_byteswap, AvbHashDescriptor};
+use core::{
+    ffi::CStr,
+    mem::{size_of, MaybeUninit},
+    str::from_utf8,
+};
+
+/// `AvbHashDescriptorFlags`; see libavb docs for details.
+pub use avb_bindgen::AvbHashDescriptorFlags as HashDescriptorFlags;
+
+/// Wraps a Hash descriptor stored in a vbmeta image.
+#[derive(Debug)]
+pub struct HashDescriptor<'a> {
+    /// Raw data in the vbmeta image.
+    pub raw_data: &'a [u8],
+
+    /// The size of the hashed image.
+    pub image_size: u64,
+
+    /// Hash algorithm name.
+    pub hash_algorithm: &'a str,
+
+    /// Flags.
+    pub flags: HashDescriptorFlags,
+
+    /// Partition name.
+    pub partition_name: &'a str,
+
+    /// Salt used to hash the image.
+    pub salt: &'a [u8],
+
+    /// Image hash digest.
+    pub digest: &'a [u8],
+}
+
+impl<'a> HashDescriptor<'a> {
+    /// Extract a `HashDescriptor` from the given descriptor contents.
+    ///
+    /// # Arguments
+    /// * `contents`: descriptor contents, including the header, in raw big-endian format.
+    ///
+    /// # Returns
+    /// The new descriptor, or `DescriptorError` if the given `contents` aren't a valid
+    /// `AvbHashDescriptor`.
+    pub(super) fn new(contents: &'a [u8]) -> DescriptorResult<Self> {
+        // Check that we can interpret `contents` as an `AvbHashDescriptor`. We don't need to check
+        // alignment because `AvbHashDescriptor` is `repr(packed)` so the compiler will properly
+        // support unaligned loads/stores.
+        if contents.len() < size_of::<AvbHashDescriptor>() {
+            return Err(DescriptorError::InvalidSize);
+        }
+
+        let header = {
+            let mut header = MaybeUninit::uninit();
+            // SAFETY:
+            // * we ensured that `contents` can be cast to an `AvbHashDescriptor` pointer
+            // * `avb_hash_descriptor_validate_and_byteswap()` checks the validity of the fields
+            //   and initializes `header` on success
+            // * even if `contents` are corrupted somehow, this will only give bogus header values
+            //   as output which will be caught below; it will never try to access memory outside
+            //   of `contents`.
+            unsafe {
+                if !avb_hash_descriptor_validate_and_byteswap(
+                    contents.as_ptr() as *const AvbHashDescriptor,
+                    header.as_mut_ptr(),
+                ) {
+                    return Err(DescriptorError::InvalidHeader);
+                }
+                header.assume_init()
+            }
+        };
+
+        // Descriptor contains: header + name + salt + digest.
+        let (_, remainder) = extract_slice(contents, size_of::<AvbHashDescriptor>())?;
+        let (partition_name, remainder) = extract_slice(remainder, header.partition_name_len)?;
+        let (salt, remainder) = extract_slice(remainder, header.salt_len)?;
+        let (digest, _) = extract_slice(remainder, header.digest_len)?;
+
+        // Extract the hash algorithm from the original header since the temporary `header`
+        // doesn't live past this function.
+        //
+        // SAFETY: we have verified that `contents` is a valid `AvbHashDescriptor`, and we
+        // hold onto a borrow of this data so it will not change while we exist.
+        let raw_header = unsafe { (contents.as_ptr() as *const AvbHashDescriptor).as_ref() };
+        let raw_header = raw_header.unwrap();
+        // The hash algorithm is a nul-terminated UTF-8 string.
+        let hash_algorithm = CStr::from_bytes_until_nul(&raw_header.hash_algorithm)
+            .map_err(|_| DescriptorError::InvalidValue)?
+            .to_str()
+            .map_err(|_| DescriptorError::InvalidUtf8)?;
+
+        Ok(Self {
+            raw_data: contents,
+            image_size: header.image_size,
+            hash_algorithm,
+            flags: HashDescriptorFlags(header.flags),
+            partition_name: from_utf8(partition_name).map_err(|_| DescriptorError::InvalidUtf8)?,
+            salt,
+            digest,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::mem::align_of;
+
+    /// A valid hash descriptor in raw big-endian format.
+    ///
+    /// It's fairly complicated to generate a descriptor programmatically, but for the purposes
+    /// of these tests we don't care about the specific values, so this is just hardcoded.
+    /// Actually extracting data from a descriptor is checked in the integration tests.
+    const TEST_HASH_DESCRIPTOR: &[u8] = &[
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0xA0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x73, 0x68, 0x61, 0x32, 0x35, 0x36,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x09,
+        0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x74, 0x65, 0x73,
+        0x74, 0x5F, 0x70, 0x61, 0x72, 0x74, 0x10, 0x00, 0x89, 0xE6, 0xFD, 0x31, 0x42, 0x91, 0x7B,
+        0x8C, 0x34, 0xAC, 0x7D, 0x30, 0x89, 0x7A, 0x90, 0x7A, 0x71, 0xBD, 0x3B, 0xF5, 0xD9, 0xB3,
+        0x9D, 0x00, 0xBF, 0x93, 0x8B, 0x41, 0xDC, 0xF3, 0xB8, 0x4F, 0x00,
+    ];
+
+    #[test]
+    fn new_hash_descriptor_success() {
+        let descriptor = HashDescriptor::new(TEST_HASH_DESCRIPTOR);
+        assert_eq!(descriptor.unwrap().raw_data, TEST_HASH_DESCRIPTOR);
+    }
+
+    #[test]
+    fn new_hash_descriptor_too_short_header_fails() {
+        let bad_header_size = size_of::<AvbHashDescriptor>() - 1;
+        assert_eq!(
+            HashDescriptor::new(&TEST_HASH_DESCRIPTOR[..bad_header_size]).unwrap_err(),
+            DescriptorError::InvalidSize
+        );
+    }
+
+    #[test]
+    fn new_hash_descriptor_too_short_contents_fails() {
+        // The last byte is padding, so we need to drop 2 bytes to trigger an error.
+        let bad_contents_size = TEST_HASH_DESCRIPTOR.len() - 2;
+        assert_eq!(
+            HashDescriptor::new(&TEST_HASH_DESCRIPTOR[..bad_contents_size]).unwrap_err(),
+            DescriptorError::InvalidSize
+        );
+    }
+
+    #[test]
+    fn hash_descriptor_is_packed() {
+        // If this test fails then we need to check for proper alignment in `HashDescriptor::new()`
+        // before we can cast a `&[u8]` to `*AvbHashDescriptor`.
+        assert_eq!(align_of::<AvbHashDescriptor>(), 1);
+    }
+}
