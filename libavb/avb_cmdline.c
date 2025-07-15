d030b05e70c6d42807929a0889f5f75ad073384c
Paul Kirth <paulkirth@google.com>
2025-07-15 15:28:07 -0700

    Fix use of uninitialized value in avb_cmdline.c
    New versions of clang will start issuing the following error:
    
    ```
    ../../third_party/android/platform/external/avb/libavb/avb_cmdline.c:351:12: error: variable 'dm_verity_mode' is used uninitialized whenever switch case is taken [-Werror,-Wsometimes-uninitialized]
      351 |       case AVB_HASHTREE_ERROR_MODE_MANAGED_RESTART_AND_EIO:
          |            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ../../third_party/android/platform/external/avb/libavb/avb_cmdline.c:358:55: note: uninitialized use occurs here
      358 |         slot_data->cmdline, "$(ANDROID_VERITY_MODE)", dm_verity_mode);
          |                                                       ^~~~~~~~~~~~~~
    ../../third_party/android/platform/external/avb/libavb/avb_cmdline.c:321:31: note: initialize the variable 'dm_verity_mode' to silence this warning
      321 |     const char* dm_verity_mode;
          |                               ^
          |                                = NULL
    1 error generated.
    ```
    
    The simplest fix is to just initialize the pointer.
    
    Change-Id: Ib9fe58b2e1073082f984cd7a17686e0b8c7db0d3

--- libavb/avb_cmdline.c
+++ libavb/avb_cmdline.c
@@ -360,7 +360,7 @@ AvbSlotVerifyResult avb_append_options(
   if (toplevel_vbmeta->flags & AVB_VBMETA_IMAGE_FLAGS_HASHTREE_DISABLED) {
     verity_mode = "disabled";
   } else {
-    const char* dm_verity_mode;
+    const char* dm_verity_mode = NULL;
     char* new_ret;
 
     switch (resolved_hashtree_error_mode) {
